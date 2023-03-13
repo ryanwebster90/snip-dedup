143a2dafb1e5dbf07ec32fa1ce7b296bb6e80274
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2023-03-13 19:30:55 +0100

    Make linter and formatter happy

--- snip_dedup/snip_compress.py
+++ snip_dedup/snip_compress.py
@@ -4,22 +4,31 @@ import os
 import os.path
 import fire
 import numpy as np
-import pandas as pd
 import glob
 import torch
 
+
 # compute features over chunks
 @torch.no_grad()
-def compute_feats_for_chunk(net,chunk,batch_size=256):
+def compute_feats_for_chunk(net, chunk, batch_size=256):
     feats = []
-    for b in range(0,chunk.shape[0],batch_size):
-        end_ind = min(b + batch_size,chunk.shape[0])
-        batch = chunk[b:end_ind,:]
+    for b in range(0, chunk.shape[0], batch_size):
+        end_ind = min(b + batch_size, chunk.shape[0])
+        batch = chunk[b:end_ind, :]
         feats += [net(torch.from_numpy(batch).float().cuda()).cpu().numpy()]
-    feats = np.concatenate(feats,axis=0)
+    feats = np.concatenate(feats, axis=0)
     return feats
 
-def snip_compress(feats_folder, snip_feats_folder='snip_feats/', model_folder='models/', snip_model='snip_vitl14_128_deep',download_models=True,feats_start=0,feats_end=-1):
+
+def snip_compress(
+    feats_folder,
+    snip_feats_folder="snip_feats/",
+    model_folder="models/",
+    snip_model="snip_vitl14_128_deep",
+    download_models=True,
+    feats_start=0,
+    feats_end=-1,
+):
     """Compress frozen clip features with SNIP
 
     Parameters
@@ -35,37 +44,40 @@ def snip_compress(feats_folder, snip_feats_folder='snip_feats/', model_folder='m
     download_models : bool, optional
         download the model and index
     """
-    
+
     if download_models:
         print("downloading snip artifact...")
-        os.makedirs(model_folder,exist_ok=True)
+        os.makedirs(model_folder, exist_ok=True)
         # download the snip net
         url = f"https://huggingface.co/datasets/fraisdufour/snip-dedup/resolve/main/models/{snip_model}.pth"
         response = requests.get(url)
-        open(os.path.join(model_folder,f'{snip_model}.pth'), "wb").write(response.content)
-    #TODO: load model
-    
+        open(os.path.join(model_folder, f"{snip_model}.pth"), "wb").write(
+            response.content
+        )
+    # TODO: load model
+
     # load snip net
-    net = torch.load(os.path.join(model_folder,f'{snip_model}.pth')).eval().cuda()
-    
-    save_dir = f'{snip_feats_folder}{snip_model}/'
-    os.makedirs(save_dir,exist_ok=True)
-    
-    #subsample feat files
-    feat_files = sorted(glob.glob(feats_folder + '/*.npy'))
+    net = torch.load(os.path.join(model_folder, f"{snip_model}.pth")).eval().cuda()
+
+    save_dir = f"{snip_feats_folder}{snip_model}/"
+    os.makedirs(save_dir, exist_ok=True)
+
+    # subsample feat files
+    feat_files = sorted(glob.glob(feats_folder + "/*.npy"))
     feats_end = len(feat_files) if feats_end < 0 else feats_end
     feat_files = feat_files[feats_start:feats_end]
-    
+
     # make snip feats folder
-    os.makedirs(snip_feats_folder,exist_ok=True)
-    
-    for ffi,ff in enumerate(feat_files):
-        print(f'computing snip feat {ffi+feats_start}/{len(feat_files)+feats_start}')
+    os.makedirs(snip_feats_folder, exist_ok=True)
+
+    for ffi, ff in enumerate(feat_files):
+        print(f"computing snip feat {ffi+feats_start}/{len(feat_files)+feats_start}")
         # this is normally the bottleneck
         feat_chunk = np.load(ff)
         # compute snip feats
-        snip_feats = compute_feats_for_chunk(net,feat_chunk)
-        np.save(f'{save_dir}{ffi+feats_start:06d}.npy',snip_feats)
+        snip_feats = compute_feats_for_chunk(net, feat_chunk)
+        np.save(f"{save_dir}{ffi+feats_start:06d}.npy", snip_feats)
+
 
 if __name__ == "__main__":
     fire.Fire(snip_compress)
--- snip_dedup/snip_index.py
+++ snip_dedup/snip_index.py
@@ -7,7 +7,17 @@ import numpy as np
 import glob
 import faiss
 
-def snip_index(feats_folder, model_folder='models/', snip_index='snip_vitl14_deep_IVFPQ_M4_base',download_models=True,feats_start=0,feats_end=-1,shard_folder='none',shard_every=1):
+
+def snip_index(
+    feats_folder,
+    model_folder="models/",
+    snip_index="snip_vitl14_deep_IVFPQ_M4_base",
+    download_models=True,
+    feats_start=0,
+    feats_end=-1,
+    shard_folder="none",
+    shard_every=1,
+):
     """Compress frozen clip features with SNIP
 
     Parameters
@@ -25,49 +35,57 @@ def snip_index(feats_folder, model_folder='models/', snip_index='snip_vitl14_dee
     shard_every : int, optional
         How often to save shards
     """
-    
+
     if download_models:
         print("downloading snip index...")
-        os.makedirs(model_folder,exist_ok=True)
+        os.makedirs(model_folder, exist_ok=True)
         url = f"https://huggingface.co/datasets/fraisdufour/snip-dedup/resolve/main/index/{snip_index}.index"
         response = requests.get(url)
-        open(os.path.join(model_folder,f'{snip_index}.index'), "wb").write(response.content)
+        open(os.path.join(model_folder, f"{snip_index}.index"), "wb").write(
+            response.content
+        )
     # TODO: just load models
-    
+
     # load snip net
-    base_index_file = os.path.join(model_folder, f'{snip_index}.index')
+    base_index_file = os.path.join(model_folder, f"{snip_index}.index")
     index = faiss.read_index(base_index_file)
-    
+
     # TODO: add option for cpu (will be quite slow however)
     res = faiss.StandardGpuResources()
-    index = faiss.index_cpu_to_gpu(res,0,index)
-    
-    #subsample feat files
-    feat_files = sorted(glob.glob(feats_folder + '/*.npy'))
+    index = faiss.index_cpu_to_gpu(res, 0, index)
+
+    # subsample feat files
+    feat_files = sorted(glob.glob(feats_folder + "/*.npy"))
     feats_end = len(feat_files) if feats_end < 0 else feats_end
     feat_files = feat_files[feats_start:feats_end]
-    
+
     # make folder to save index shards
-    if shard_folder == 'none':
-        shard_folder = os.path.join(model_folder,f'{snip_index}_shards/')
-    
+    if shard_folder == "none":
+        shard_folder = os.path.join(model_folder, f"{snip_index}_shards/")
+
     # make snip feats folder
-    os.makedirs(f'{shard_folder}',exist_ok=True)
-    
-    for ffi,ff in enumerate(feat_files):
-        print(f'adding snip feat {ffi+feats_start}/{len(feat_files)+feats_start} to index')
+    os.makedirs(f"{shard_folder}", exist_ok=True)
+
+    for ffi, ff in enumerate(feat_files):
+        print(
+            f"adding snip feat {ffi+feats_start}/{len(feat_files)+feats_start} to index"
+        )
         # this is normally the bottleneck
         feat_chunk = np.load(ff)
-        
+
         # add to index (todo: benchmark and progress)
         index.add(feat_chunk)
-        
-        if (ffi+1)%shard_every==0:
+
+        if (ffi + 1) % shard_every == 0:
             # write index shard, name according to the feature files within the shard
-            faiss.write_index(faiss.index_gpu_to_cpu(index), f'{shard_folder}{ffi + feats_start - shard_every + 1}_{ffi + feats_start + 1}_shard.index')
-            
+            faiss.write_index(
+                faiss.index_gpu_to_cpu(index),
+                f"{shard_folder}{ffi + feats_start - shard_every + 1}_{ffi + feats_start + 1}_shard.index",
+            )
+
             # re-load the base index for the new shard
             index = faiss.read_index(base_index_file)
-            
+
+
 if __name__ == "__main__":
     fire.Fire(snip_index)
