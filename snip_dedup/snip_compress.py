c6eba8f8c27360f3df2c16c8ca1898e1ccb0b43f
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2023-03-15 17:17:32 +0100

    Improve snip_compress doc and args validation

--- snip_dedup/snip_compress.py
+++ snip_dedup/snip_compress.py
@@ -2,28 +2,33 @@
 import os
 import os.path
 import sys
+import string
 import fire
 import numpy as np
 import torch
 
 
 def _validate_shards(shards):
+    if type(shards) is int:
+        sys.exit(
+            "Single value is not accepted for --shards as it's ambiguous between wanting only that exact shard or that number of shards starting from 0. Please use a range instead like 0:2"
+        )
     shards_bounds = shards.split(":")
     try:
         shards_bounds = [int(shard) for shard in shards_bounds]
     except Exception:
         sys.exit(
-            f'The shards pattern "{shards}" is not valid. It should be a valid range such as "0:" or "14:42"'
+            f'The shards pattern "{shards}" is not valid. It should be a valid range such as "0:2" or "14:42"'
         )
     if len(shards_bounds) == 0:
         sys.exit("The --shards argument cannot be empty")
     elif len(shards_bounds) == 1:
         sys.exit(
-            "Single value is not accepted for --shards as it's ambiguous between wanting only that exact shard or that number of shards starting from 0."
+            "Single value is not accepted for --shards as it's ambiguous between wanting only that exact shard or that number of shards starting from 0. Please use a range instead like 0:2"
         )
     elif len(shards_bounds) > 2:
         sys.exit(
-            "Ranges with more than 2 parts, such as 0:2:14 are not valid for --shard. Please limit yourself with simple ranges such as 0:14"
+            "Ranges with more than 2 parts, such as 0:2:14 are not valid for --shards. Please limit yourself with simple ranges such as 0:14"
         )
     start_shard, end_shard = shards_bounds
     if start_shard < 0 or end_shard < 0:
@@ -33,6 +38,16 @@ def _validate_shards(shards):
             'The --shards argument must be of the shape "s:e" with s < e, such as "0:1" or "14:42". The "e" bound is excluded.'
         )
     return start_shard, end_shard
+
+
+def _validate_shard_format(pattern):
+    format_variables = [
+        tup[1] for tup in string.Formatter().parse(pattern) if tup[1] is not None
+    ]
+    if len(format_variables) != 1 or format_variables[0] != "shard":
+        sys.exit(
+            f'Your pattern "{pattern}" is not valid as it should contain the "shard" variable such as "{{shard:04d}}.npy".'
+        )
 
 
 # compute features over chunks
@@ -53,20 +68,20 @@ def snip_compress(
     clip_feats="clip_feats/{shard:04d}.npy",
     snip_feats_out="snip_feats/{shard:04d}.npy",
 ):
-    """Compress frozen clip features with SNIP
+    """Compress frozen CLIP features with SNIP
 
     Parameters
     ----------
-    feats_folder : str
-        folder with .npy clip features
-    model_folder : str
-        folder with snip models
-    snip_model : str, optional
-        Snip model artifact which compresses feats (see available models at fraisdufour/snip-dedup on hf)
-    snip_index : str, optional
-        snip index built on top of compressed feats
-    download_models : bool, optional
-        download the model and index
+    snip_model_path : str
+        Path to the SNIP model file to use, might be something like: snip_models/snip_vitl14_128_deep.pth
+    shards : str
+        Shards to compress, using a slice notation, such as 0:2 or 14:42
+    clip_feats : str
+        Pattern referencing the path to the CLIP features shards.
+        You are expected to use the "shard" variable with formatting options, such as "{shard:03d}.npy" which will be replaced by "001.npy" when shard==1.
+    snip_feats_out : str
+        Pattern referencing the path to the SNIP compressed features shards that will be computed.
+        You are expected to use the "shard" variable with formatting options, such as "{shard:03d}.npy" which will be replaced by "001.npy" when shard==1.
     """
     # Check that the SNIP model path passed as argument is valid
     if not os.path.isfile(snip_model_path):
@@ -78,6 +93,7 @@ def snip_compress(
     start_shard, end_shard = _validate_shards(shards)
 
     # Check that the CLIP features exist
+    _validate_shard_format(clip_feats)
     for shard in range(start_shard, end_shard):
         clip_shard_path = clip_feats.format(shard=shard)
         if not os.path.isfile(clip_shard_path):
@@ -86,6 +102,7 @@ def snip_compress(
             )
 
     # Create directory for the computed SNIP shards
+    _validate_shard_format(snip_feats_out)
     try:
         first_snip_shard_path = snip_feats_out.format(shard=start_shard)
         snip_out_parent_dir = os.path.dirname(first_snip_shard_path)
