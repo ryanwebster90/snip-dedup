8e0460e3a2bc8ca996657e0a3966c6aee703c511
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2023-03-15 14:00:19 +0100

    Cleanup on snip_compress

--- snip_dedup/snip_compress.py
+++ snip_dedup/snip_compress.py
@@ -1,11 +1,38 @@
 """snip compress"""
-import requests
 import os
 import os.path
+import sys
 import fire
 import numpy as np
-import glob
 import torch
+
+
+def _validate_shards(shards):
+    shards_bounds = shards.split(":")
+    try:
+        shards_bounds = [int(shard) for shard in shards_bounds]
+    except:
+        sys.exit(
+            f'The shards pattern "{shards}" is not valid. It should be a valid range such as "0:" or "14:42"'
+        )
+    if len(shards_bounds) == 0:
+        sys.exit("The --shards argument cannot be empty")
+    elif len(shards_bounds) == 1:
+        sys.exit(
+            "Single value is not accepted for --shards as it's ambiguous between wanting only that exact shard or that number of shards starting from 0."
+        )
+    elif len(shards_bounds) > 2:
+        sys.exit(
+            "Ranges with more than 2 parts, such as 0:2:14 are not valid for --shard. Please limit yourself with simple ranges such as 0:14"
+        )
+    start_shard, end_shard = shards_bounds
+    if start_shard < 0 or end_shard < 0:
+        sys.exit("Only positive integers are allowed for --shards, such as 0:14")
+    if end_shard <= start_shard:
+        sys.exit(
+            'The --shards argument must be of the shape "s:e" with s < e, such as "0:1" or "14:42". The "e" bound is excluded.'
+        )
+    return start_shard, end_shard
 
 
 # compute features over chunks
@@ -21,13 +48,10 @@ def compute_feats_for_chunk(net, chunk, batch_size=256):
 
 
 def snip_compress(
-    feats_folder,
-    snip_feats_folder="snip_feats/",
-    model_folder="models/",
-    snip_model="snip_vitl14_128_deep",
-    download_models=True,
-    feats_start=0,
-    feats_end=-1,
+    snip_model_path,
+    shards="0:2",
+    clip_feats="clip_feats/{shard:04d}.npy",
+    snip_feats_out="snip_feats/{shard:04d}.npy",
 ):
     """Compress frozen clip features with SNIP
 
@@ -44,39 +68,47 @@ def snip_compress(
     download_models : bool, optional
         download the model and index
     """
-
-    if download_models:
-        print("downloading snip artifact...")
-        os.makedirs(model_folder, exist_ok=True)
-        # download the snip net
-        url = f"https://huggingface.co/datasets/fraisdufour/snip-dedup/resolve/main/models/{snip_model}.pth"
-        response = requests.get(url)
-        open(os.path.join(model_folder, f"{snip_model}.pth"), "wb").write(
-            response.content
+    # Check that the SNIP model path passed as argument is valid
+    if not os.path.isfile(snip_model_path):
+        sys.exit(
+            f'The SNIP model file "{snip_model_path}" does not exist or is not readable.'
         )
-    # TODO: load model
 
-    # load snip net
-    net = torch.load(os.path.join(model_folder, f"{snip_model}.pth")).eval().cuda()
+    # Check that the shards argument is correct
+    start_shard, end_shard = _validate_shards(shards)
 
-    save_dir = f"{snip_feats_folder}{snip_model}/"
-    os.makedirs(save_dir, exist_ok=True)
+    # Check that the CLIP features exist
+    for shard in range(start_shard, end_shard):
+        clip_shard_path = clip_feats.format(shard=shard)
+        if not os.path.isfile(clip_shard_path):
+            sys.exit(
+                f'The CLIP file for shard {shard} does not exist: "{clip_shard_path}"'
+            )
 
-    # subsample feat files
-    feat_files = sorted(glob.glob(feats_folder + "/*.npy"))
-    feats_end = len(feat_files) if feats_end < 0 else feats_end
-    feat_files = feat_files[feats_start:feats_end]
+    # Create directory for the computed SNIP shards
+    try:
+        first_snip_shard_path = snip_feats_out.format(shard=start_shard)
+        snip_out_parent_dir = os.path.dirname(first_snip_shard_path)
+        os.makedirs(snip_out_parent_dir, exist_ok=True)
+    except:
+        sys.exit(
+            f'Something is wrong with the output file paths specified for --snip_feats_out "{snip_feats_out}"'
+        )
 
-    # make snip feats folder
-    os.makedirs(snip_feats_folder, exist_ok=True)
+    # Load SNIP net
+    net = torch.load(snip_model_path).eval().cuda()
 
-    for ffi, ff in enumerate(feat_files):
-        print(f"computing snip feat {ffi+feats_start}/{len(feat_files)+feats_start}")
+    # Compute SNIP features for all shards
+    print(f"Start computing SNIP features for shards {start_shard} to {end_shard} (excluded)")
+    for shard in range(start_shard, end_shard):
+        print(f"  Computing SNIP features for shard {shard} ...")
         # this is normally the bottleneck
-        feat_chunk = np.load(ff)
-        # compute snip feats
-        snip_feats = compute_feats_for_chunk(net, feat_chunk)
-        np.save(f"{save_dir}{ffi+feats_start:06d}.npy", snip_feats)
+        clip_shard_path = clip_feats.format(shard=shard)
+        clip_shard = np.load(clip_shard_path)
+        # compute SNIP features
+        snip_feats = compute_feats_for_chunk(net, clip_shard)
+        snip_shard_path = snip_feats_out.format(shard=shard)
+        np.save(snip_shard_path, snip_feats)
 
 
 if __name__ == "__main__":
